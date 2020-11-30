32a270f98064daa9df77c6e95f69e629acf0bc80
ZZC19951024 <zhu.zic@husky.neu.edu>
2020-11-30 09:34:56 +0800

    Updates

---
+++ precache-manifest.9bd674a7a291eb892206664520070dd3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb89e78f30887beed815fdf83afa871",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "9503bcbece431ac98710",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.3ef9ebeb.chunk.css"
+  },
+  {
+    "revision": "7ab5a0b6e49348f77d47",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.54f4046b.chunk.js"
+  },
+  {
+    "revision": "a679815cc6fac19ef3aba38874430d48",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.54f4046b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9503bcbece431ac98710",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.54bd7370.chunk.js"
+  },
+  {
+    "revision": "c7a57ff503183f52bf24",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime-main.32c1fe33.js"
+  },
+  {
+    "revision": "3cd3cfa6a2e9e746f586b67120d661fe",
+    "url": "/seainfo6150-final-project-webapp/static/media/Rowdies-Bold.3cd3cfa6.ttf"
+  },
+  {
+    "revision": "e4558190154456c237c0432c380ef08b",
+    "url": "/seainfo6150-final-project-webapp/static/media/Rowdies-Light.e4558190.ttf"
+  },
+  {
+    "revision": "b2a024dc5716bc92b5496d87f05ad216",
+    "url": "/seainfo6150-final-project-webapp/static/media/Rowdies-Regular.b2a024dc.ttf"
+  },
+  {
+    "revision": "82c3403ddfd167db2dea4b343f85f223",
+    "url": "/seainfo6150-final-project-webapp/static/media/logo.82c3403d.png"
+  },
+  {
+    "revision": "94d6cfdd338f690dda5ab6ca1673b19d",
+    "url": "/seainfo6150-final-project-webapp/static/media/raven.94d6cfdd.svg"
+  }
+]);
